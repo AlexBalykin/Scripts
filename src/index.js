a617b3d219e67566fa199878265d9bbac3711e89
Alex Balykin <alexander.balykin1@gmail.com>
2021-02-11 17:38:36 +0300

    if

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ const regexp = (str) => str.replace(/\r?\n/g, "','").split('\n').join().trim();
 const script = {
   getTransactionIdByCardId: (file) => {
     const data1 = getPath(file).split('\n');
-    const awk = data1.map((i) => i.split('\t')[2]);
-    fs.writeFileSync('awk', awk.join('\n'));
-
-    const data = getPath('awk').split(',');
+    if (data1[0].length > 40) {
+      const awk = data1.map((i) => i.split('\t')[2]);
+      fs.writeFileSync('awk', awk.join('\n'));
+      const data = getPath('awk').split(',');
+      const query = `SELECT t.id, t.REGION_ID, ts."name" AS Статус, cdts."name" AS Статус_долга, ct."name" AS Карта FROM BILLING."transaction" t LEFT JOIN CARD_DEBT_TRANSACTION cd ON t.ID = cd.TRANSACTION_ID LEFT JOIN TRANSACTION_STATUS TS ON t.STATUS_ID = ts.ID LEFT JOIN CARD_DEBT_TRANSACTION_STATUS CDTS ON cd.STATUS_ID = cdts.ID LEFT JOIN CARD C ON t.CARD_ID=c.ID LEFT JOIN CARD_TYPE CT ON ct.ID=c.TYPE_ID WHERE t.CARD_ID in ('${data}') AND t.STATUS_ID IN (1,2,4) and cd.STATUS_ID IN (1,2,4);`;
+      const result = regexp(query);
+      fs.unlinkSync(path.resolve('awk'));
+      return result;
+    }
+    const data = getPath(file).split(',');
     const query = `SELECT t.id, t.REGION_ID, ts."name" AS Статус, cdts."name" AS Статус_долга, ct."name" AS Карта FROM BILLING."transaction" t LEFT JOIN CARD_DEBT_TRANSACTION cd ON t.ID = cd.TRANSACTION_ID LEFT JOIN TRANSACTION_STATUS TS ON t.STATUS_ID = ts.ID LEFT JOIN CARD_DEBT_TRANSACTION_STATUS CDTS ON cd.STATUS_ID = cdts.ID LEFT JOIN CARD C ON t.CARD_ID=c.ID LEFT JOIN CARD_TYPE CT ON ct.ID=c.TYPE_ID WHERE t.CARD_ID in ('${data}') AND t.STATUS_ID IN (1,2,4) and cd.STATUS_ID IN (1,2,4);`;
     const result = regexp(query);
-    fs.unlinkSync(path.resolve('awk'));
     return result;
   },
 
